37f39acffa38a56b262b59ef6924fb06f03f0540
SIELA <knowanegg@outlook.com>
2022-07-01 17:17:46 +0800

    fix:修复注释说明中的拼写错误

--- ip_pingscan_with_param.bat
+++ ip_pingscan_with_param.bat
@@ -1,6 +1,6 @@
 ::使用ping扫描内网c段,
 ::使用方法: .\ip_pingscan_with_param 192.168.1
-::ping通的主机放在当前执行目录的ip_success.txt,ping不通的放在ip_failed
+::ping通的主机放在当前执行目录的ip_success.txt
 ::注：
 ::  ping /n 1 /w 40 表示只ping一次,丢弃40ms以上的请求，保证扫描结果为内网
 ::  &&作用是前面语句执行成功后才执行后面的语句
