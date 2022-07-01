77899a2a30885ca30fecbc339e0a1311052e90c7
SIELA <502605617@qq.com>
2022-07-01 17:05:17 +0800

    fix:修复注释说明中的拼写错误

--- ip_pingscan_with_param.bat
+++ ip_pingscan_with_param.bat
@@ -3,6 +3,6 @@
 ::ping通的主机放在当前执行目录的ip_success.txt,ping不通的放在ip_failed
 ::注：
 ::  ping /n 1 /w 40 表示只ping一次,丢弃40ms以上的请求，保证扫描结果为内网
-::  &&作用是只有ping通才执行后门的写入语句
+::  &&作用是前面语句执行成功后才执行后面的语句
 
-@for /l %%i in (1,1,255) do @ping /n 1 /w 40 %1.%%i && echo %1.%%i>>ip_success.txt
+@for /l %%i in (1,1,255) do ping /n 1 /w 40 %1.%%i && echo %1.%%i>>ip_success.txt
