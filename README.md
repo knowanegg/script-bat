4a6333d55d8a0e473a1004be8e7ff2d386ef3683
SIELA <502605617@qq.com>
2022-07-01 16:38:41 +0800

    feat:添加ip_pingscan_with_params.bat脚本
    feat:ip_pingscan_with_params.bat -- 对指定的ip地址C段使用ping进行扫描探测的脚本，使用参数方式传入IP地址段

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# script-bat
- bat格式的扫描脚本
+# script-bat (bat格式的脚本)
+
+## ip_pingscan_with_params.bat
+对指定的ip地址C段使用ping进行扫描探测，使用参数方式传入IP地址段  
+
+使用方法:  
+`./ip_pingscan_with_params.bat 192.168.1`
+
+--- 
+
---
+++ ip_pingscan_with_param.bat
@@ -0,0 +1,8 @@
+::使用ping扫描内网c段,
+::使用方法: .\ip_pingscan_with_params 192.168.1
+::ping通的主机放在当前执行目录的ip_success.txt,ping不通的放在ip_failed
+::注：
+::  ping /n 1 /w 40 表示只ping一次,丢弃40ms以上的请求，保证扫描结果为内网
+::  &&作用是只有ping通才执行后门的写入语句
+
+@for /l %%i in (1,1,255) do @ping /n 1 /w 40 %1.%%i && echo %1.%%i>>ip_success.txt
